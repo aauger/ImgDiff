c44515088bbff0f983004fc2019e1ce70281b2b0
aauger <augera09@students.rowan.edu>
2017-07-25 20:19:49 -0400

    Initial commit
    Initial source, no error checking (except dimensions).

---
+++ ImgDiff/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ImgDiff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diffMult = 20;
+
+            if (args.Length < 3)
+                Console.WriteLine("Usage: img1 img2 output [multiplier]");
+            if (args.Length == 4)
+                diffMult = int.Parse(args[3]);
+
+            Bitmap fInput = (Bitmap)Image.FromFile(args[0]);
+            Bitmap sInput = (Bitmap)Image.FromFile(args[1]);
+            if (fInput.Width != sInput.Width || fInput.Height != sInput.Height)
+                Environment.Exit(1);
+            Bitmap result = new Bitmap(fInput.Width, fInput.Height);
+            String outputFn = args[2];
+            if (!outputFn.ToLower().EndsWith(".png"))
+                outputFn += ".png";
+
+            for (int x = 0; x < fInput.Width; x++)
+            {
+                for (int y = 0; y < fInput.Height; y++)
+                {
+                    result.SetPixel(x, y, DiffColor(
+                        fInput.GetPixel(x, y),
+                        sInput.GetPixel(x, y),
+                        diffMult));
+                }
+            }
+
+            result.Save(outputFn, System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        static int RGBClamp(int val)
+        {
+            if (val < 0)
+                return 0;
+            if (val > 255)
+                return 255;
+            return val;
+        }
+
+        static Color DiffColor(Color a, Color b, int mult)
+        {
+            return Color.FromArgb(
+                    RGBClamp(Math.Abs(a.R - b.R) * mult),
+                    RGBClamp(Math.Abs(a.G - b.G) * mult),
+                    RGBClamp(Math.Abs(a.B - b.B) * mult)
+                );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ImgDiff
+Functionally the same as what you'd get with ImageMagick, but simpler to use. Provide two images and a filename, and the output will be the diff of the two images.
+
+Input order matters, but the difference is probably not material.
+
+###Example
+####PNG:
+![First image](http://i.imgur.com/5zg6woY.png)
+####JPG:
+![Second image](http://i.imgur.com/rx7Gc2m.jpg)
+####Diff:
+![Diff image](http://i.imgur.com/k4TgjAf.png)
